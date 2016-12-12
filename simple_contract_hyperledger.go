39926815787d2eb50dbbe7bdfbede030940d65ba
Anzal <anzalbeg24494@gmail.com>
2016-12-12 13:51:30 +0530

    Update simple_contract_hyperledger.go

--- simple_contract_hyperledger.go
+++ simple_contract_hyperledger.go
@@ -59,9 +59,14 @@ type Geolocation struct {
     Longitude   *float64 `json:"longitude,omitempty"`
 }
 type AssetState struct {
-    AssetID        *string       `json:"assetID,omitempty"`        // all assets must have an ID, primary key of contract
-    Location       *Geolocation  `json:"location,omitempty"`       // current asset location
-    Status        *string       `json:"kitstatus,omitempty"`        // the name of the carrier
+    AssetID         		*string       `json:"assetID,omitempty"`        // all assets must have an ID, primary key of contract
+    Location        		*Geolocation  `json:"location,omitempty"`       // current asset location
+    Status          		*string       `json:"kitstatus,omitempty"`        // the name of the carrier
+	Role            		*string       `json:"role,omitempty"` 
+	Qwnername       		*string       `json:"ownername,omitempty"` 
+	Qwnerid         		*string       `json:"ownerid,omitempty"` 
+	overalltransaction      *string       `json:"overalltransaction,omitempty"` 
+	
 }
 var contractState = ContractState{MYVERSION}
 
