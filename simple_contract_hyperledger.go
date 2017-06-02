853e660f028110c759d8c3656dd8b99eb85a2834
ruphatak <rucha.a.phatak@capgemini.com>
2017-06-02 12:22:49 +0530

    Update simple_contract_hyperledger.go

--- simple_contract_hyperledger.go
+++ simple_contract_hyperledger.go
@@ -213,7 +213,8 @@ func (t *SimpleChaincode) deleteAsset(stub shim.ChaincodeStubInterface, args []s
  func (t *SimpleChaincode) readContainerHistory(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
     var assetID string                  // asset ID
     var state = AssetState{} // The calling function is expecting an object of type AssetState
-	assetID = ""
+    var err error
+    assetID = ""
     if len(args) !=1 {
         err = errors.New("Incorrect number of arguments. Expecting a single JSON string with mandatory Container Number")
 		//fmt.Println(err)
